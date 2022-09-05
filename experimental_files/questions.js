fc580be6b92705b856910ddaf8b5b9d70ea92a85
csavasegal <csavasegal@gmail.com>
2022-09-05 13:57:32 -0400

    added in experimental files

---
+++ experimental_files/questions.js
@@ -0,0 +1,61 @@
+
+defined_questions= [
+    { // characters
+        "Defeat": ['the mother','the sister','the brother'],
+        "Growth": ['the mother','the father','the younger brother','the older brother'],
+        "Lemonade": ['the machine'],
+        "Iteration": ['the woman', 'the man']
+
+    },
+    {//MC question one
+        "Defeat": ['Who does the brother take a seat on the bus from?'],
+        "Growth": ['What triggered the parents\' first fight?'], //check this
+        "Lemonade": ['What room did the video start in?'],
+        "Iteration": ['What is the first thing that happens every time the woman wakes up?']  
+    },
+    { //MC question one responses
+        "Defeat": ['His sister','A child with crutches','The teacher'],
+        "Growth": ['The process of renovating the house','The children making a mess','Forgetting Valentine\'s Day'],
+        "Lemonade": ['Kitchen','Living room','Bedroom'],
+        "Iteration": ['The door opens','A glass falls off the table','She screams']
+    },
+    {//MC question two
+        "Defeat": ['What sport do the children play?'],
+        "Growth": ['What was the first indication of non-monogamy?'],
+        "Lemonade": ['Which of the following was NOT used in the machine?'],
+        "Iteration": ['What does the woman notice the first time she sees her past self?']  
+    },
+    { //MC question two responses
+        "Defeat": ['Soccer','Lacrosse','Track'],
+        "Growth": ['A bra on the floor','The mother isn\'t home after work','A woman is seen undressing in the window of the house across the street'],
+        "Lemonade": ['Stuffed animal','Dominoes','Plates'],
+        "Iteration": ['A tattoo on the back of her neck','A tracking device','Her hair color']
+    },
+    {//MC question three
+        "Defeat": ['What does the brother do before he is called down for dinner and sees his sister there first?'],
+        "Growth": ['At what holiday dinner does the father yell at the older son?'],
+        "Lemonade": ['How many neon fidget spinners are lined up before the slime drips from the cup?'],
+        "Iteration": ['What happens to the cut in the tree?']  
+    },
+    { //MC question three responses
+        "Defeat": ['He is taking a nap','He is playing video games','He is doing his homework'],
+        "Growth": ['Thanksgiving','Halloween','Christmas'],
+        "Lemonade": ['2','3','5'],
+        "Iteration": ['The wood begins to crumble','The wood bleeds','The cut heals itself']
+    },
+    {//MC question four
+        "Defeat": ['Why does the brother first suspect something weird is going on?'],
+        "Growth": ['What does the younger brother do with the painting after he takes it off the wall?'],
+        "Lemonade": ['What was used most often in the video?'],
+        "Iteration": ['What does the woman do the cycle after the cut in the tree heals?']  
+    },
+    { //MC question four responses
+        "Defeat": ['He hears noises from his sister\'s room','The lights flicker on and off','He notices that he doesn\'t have as much food'],
+        "Growth": ['He steals it','He burns it','He throws it away'],
+        "Lemonade": ['Balls','Legos','Lemons'],
+        "Iteration": ['She screams for help','She breaks the water glass','She goes back to sleep']
+    },
+    {
+        "Iteration": ['the woman', 'the man']
+    }
+]
